b1c3e7776ebad31cb6a3e5f0cb0b2808985eafd0
Mike <staff@mystickrewe.com>
2021-03-24 15:33:24 -0500

    update

---
+++ set-symlinks.bat
@@ -0,0 +1,7 @@
+mklink c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.cpp c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.cpp
+mklink c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.h c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.
+
+mklink c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.cpp c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.cpp
+mklink c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.h c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.h
+
+
