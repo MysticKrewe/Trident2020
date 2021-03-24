a0290ea0ca793c880cae55e2c42e118737fc46e9
Mike <staff@mystickrewe.com>
2021-03-24 15:43:22 -0500

    hard symlinks

--- set-symlinks.bat
+++ set-symlinks.bat
@@ -1,7 +1,9 @@
-mklink c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.cpp c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.cpp
-mklink c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.h c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.
 
-mklink c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.cpp c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.cpp
-mklink c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.h c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.h
+
+mklink /H c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.cpp c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.cpp
+mklink /H c:\mike\pinball-arduino35\machine-work\Trident2020\BallySternOS.h c:\mike\pinball-arduino35\git\BallySternOS\BallySternOS.h
+
+mklink /H c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.cpp c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.cpp
+mklink /H c:\mike\pinball-arduino35\machine-work\Trident2020\SelfTestAndAudit.h c:\mike\pinball-arduino35\git\BallySternOS\SelfTestAndAudit.h
 
 
